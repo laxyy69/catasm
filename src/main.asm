0ab38c3840b7d75b2d0672c8f011b71f919e4c64
Your Name <you@example.com>
2024-05-01 18:48:14 +0200

    Initial src/main.asm

---
+++ src/main.asm
@@ -0,0 +1,111 @@
+global _start
+
+%define SYS_READ    0x00
+%define SYS_WRITE   0x01
+%define SYS_OPEN    0x02
+%define SYS_CLOSE   0x03
+%define SYS_FSTAT   0x05
+%define SYS_EXIT    0x3c
+
+; open() flags
+%define O_RDONLY    00
+%define O_WRONLY    01
+%define O_RDWR      02
+
+section .bss
+struct_stat: resb 144
+argc: resb 8
+argv: resb 8
+
+section .text
+; exit(rdi)
+exit:
+    mov     rax, SYS_EXIT
+    syscall
+
+; open(filename: const char* (rdi), flags: int (rsi))
+open:
+    mov     rax, SYS_OPEN
+    syscall
+    ret
+
+; close(fd: int (rdi))
+close:
+    mov     rax, SYS_CLOSE
+    syscall
+    ret
+
+; read(fd: int (rdi), buf: void* (rsi), len: size_t (rdx))
+read:
+    mov     rax, SYS_READ
+    syscall
+    ret
+
+; write(fd: int (rdi), buf: const void* (rsi), len: size_t (rdx))
+write:
+    mov     rax, SYS_WRITE
+    syscall
+    ret
+
+; fstat(fd: int (rdi), struct stat* (rsi))
+fstat:
+    mov     rax, SYS_FSTAT
+    syscall
+    ret
+
+main:
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 16
+
+    ; open(file, O_RDONLY)
+    mov     rdi, [argv]
+    mov     rsi, O_RDONLY
+    call    open
+    mov     DWORD [rbp - 0x04], eax
+
+    mov     edi, DWORD [rbp - 0x04]
+    mov     rsi, struct_stat
+    call    fstat
+
+    ; struct_stat.st_size
+    mov     r9, [struct_stat + 0x30]
+    mov     [rbp - 16], r9
+    ; allocate stack memory for file
+    sub     rsp, r9
+
+    ; read(fd, ptr, 10)
+    mov     edi, DWORD [rbp - 0x04]
+    lea     rsi, [rsp]
+    mov     rdx, [rbp - 16]
+    call    read
+    mov     [rbp - 0x08], eax
+
+    ; write(1, ptr, n)
+    mov     rdi, 0x01
+    lea     rsi, [rsp]
+    mov     rdx, [rbp - 16]
+    call    write
+
+    add     rsp, r9
+
+    mov     edi, DWORD [rbp - 0x04]
+    call    close
+
+    add     rsp, 16
+    pop     rbp
+    ret
+
+_start:
+    ; argc
+    pop     rax
+    mov     [argc], rax
+    pop     rax
+
+    add     rax, 8
+    mov     [argv], rax
+
+    call    main
+
+    mov     rdi, 0x00
+    jmp     exit
